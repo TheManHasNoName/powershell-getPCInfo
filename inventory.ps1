4e0e9023f8d3d5eed922bc729cc56542cff234f0
unknown <xavierjohanis@gmail.com>
2019-11-14 22:44:13 +0800

    first

---
+++ inventory.ps1
@@ -0,0 +1,21 @@
+﻿$pcs = Get-Content -Path 'C:\Users\Xavier Johanis\Desktop\PC Inventory\pc.txt'
+$active = Get-Content -Path 'C:\Users\Xavier Johanis\Desktop\PC Inventory\active.txt'
+$info = Get-Content -Path 'C:\Users\Xavier Johanis\Desktop\PC Inventory\info.txt'
+
+$adminaccount = $domain + "\administrator"
+$PASSWORD = ConvertTo-SecureString "P@ssw0rd" -AsPlainText -Force
+$UNPASSWORD = New-Object System.Management.Automation.PsCredential $adminaccount, $PASSWORD
+
+foreach ($computer in $pcs) {
+    if (Test-Connection -ComputerName $computer -Quiet -Count 2) {
+        Add-Content -value $computer -Path 'C:\Users\Xavier Johanis\Desktop\PC Inventory\active.txt'
+    }
+}
+
+
+foreach ($computer in $active) {
+    $OS = Get-WmiObject Win32_OperatingSystem -Computername $computer -Credential $UNPASSWORD
+#    $Networks = Get-WmiObject Win32_NetworkAdapterConfiguration -ComputerName $computer | Where-Object {$_.IPEnabled}
+#    $IPAddress  = $Networks.IpAddress[0]
+#    $MACAddress  = $Networks.MACAddress
+}
